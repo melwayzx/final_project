ae4f51ec91b4f6a962ffa050bf5e739997540a98
Melwayz's <>
2020-07-01 15:32:48 +0700

    Create covid-19 form button

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Button() {
+  return <div style={StyledButtonContainer}>แบบประเมินความเสี่ยง</div>;
+}
+
+const StyledButtonContainer = {
+  width: "200px",
+  backgroundColor: "#C23D3D",
+  borderRadius: "8px",
+  color: "white",
+  padding: "10px",
+  boxSixing: "border-box",
+  textAlign: "center",
+};
--- src/components/LandingCard.js
+++ src/components/LandingCard.js
@@ -1,27 +1,31 @@
 import StatusCard from "./Card/StatusCard";
-import { colors } from "@material-ui/core";
+import Button from "../components/Button";
 
 export default function LandingCard({ domesticDailyCase }) {
   console.log(domesticDailyCase);
   return (
     <div>
       <div style={StyledContainer}>
-        <div style={StyledTextContainer}>
-          <div style={StyledText}>อัปเดตสถิติ </div>
-          <div
-            style={{
-              ...StyledText,
-              color: "#C23D3D",
-              fontSize: "60px",
-              fontWeight: 900,
-              lineHeight: "45px",
-            }}
-          >
-            {" "}
-            COVID-19
+        <div style={StyledWrapper}>
+          <div style={StyledTextContainer}>
+            <div style={StyledText}>อัปเดตสถิติ </div>
+            <div
+              style={{
+                ...StyledText,
+                color: "#C23D3D",
+                fontSize: "60px",
+                fontWeight: 900,
+                lineHeight: "45px",
+              }}
+            >
+              {" "}
+              COVID-19
+            </div>
+            <div style={StyledText}>ประเทศไทย </div>
           </div>
-          <div style={StyledText}>ประเทศไทย </div>
+          <Button />
         </div>
+
         <StatusCard domesticDailyCase={domesticDailyCase} />
       </div>
     </div>
@@ -42,5 +46,12 @@ const StyledTextContainer = {
   flexDirection: "column",
   justifyContent: "center",
   textAlign: "center",
+};
+
+const StyledWrapper = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
   marginRight: "100px",
 };
