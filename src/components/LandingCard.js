11eb98e39898a07a6f9727fb3f1de01a3c3a7559
Melwayz's <>
2020-07-01 15:02:54 +0700

    Edit title text

--- src/components/LandingCard.js
+++ src/components/LandingCard.js
@@ -1,11 +1,24 @@
 import StatusCard from "./Card/StatusCard";
+import { colors } from "@material-ui/core";
 
 export default function LandingCard({ domesticDailyCase }) {
   return (
     <div>
       <div style={StyledContainer}>
         <div style={StyledTextContainer}>
-          <div style={StyledText}>อัปเดตสถิติ COVID-1 </div>
+          <div style={StyledText}>อัปเดตสถิติ </div>
+          <div
+            style={{
+              ...StyledText,
+              color: "#C23D3D",
+              fontSize: "60px",
+              fontWeight: 900,
+              lineHeight: "45px",
+            }}
+          >
+            {" "}
+            COVID-19
+          </div>
           <div style={StyledText}>ประเทศไทย </div>
         </div>
         <StatusCard domesticDailyCase={domesticDailyCase} />
@@ -20,9 +33,13 @@ const StyledContainer = {
 
 const StyledText = {
   fontFamily: "Sukhumvit Set",
-  fontSize: "50px",
+  fontSize: "40px",
 };
 
 const StyledTextContainer = {
+  display: "flex",
+  flexDirection: "column",
   justifyContent: "center",
+  textAlign: "center",
+  marginRight: "100px",
 };
