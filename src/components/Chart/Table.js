d5b2013851415eb329d28e598b7020e250155451
Melwayz's <>
2020-07-02 18:00:19 +0700

    Edit component wrapper

--- src/components/Chart/Table.js
+++ src/components/Chart/Table.js
@@ -6,11 +6,11 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+// import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 600,
+    width: 400,
   },
 });
 
@@ -20,25 +20,25 @@ export default function SimpleTable({ data }) {
   const provinceList = Object.keys(data.Province);
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>จังหวัด</TableCell>
-            <TableCell align="right">ผู้ติดเชื้อ</TableCell>
+    // <TableContainer component={Paper}>
+    <Table className={classes.table} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>จังหวัด</TableCell>
+          <TableCell align="right">ผู้ติดเชื้อ</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {provinceList.map((province) => (
+          <TableRow key={province}>
+            <TableCell component="th" scope="row">
+              {province}
+            </TableCell>
+            <TableCell align="right">{data.Province[province]}</TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {provinceList.map((province) => (
-            <TableRow key={province}>
-              <TableCell component="th" scope="row">
-                {province}
-              </TableCell>
-              <TableCell align="right">{data.Province[province]}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        ))}
+      </TableBody>
+    </Table>
+    // </TableContainer>
   );
 }
