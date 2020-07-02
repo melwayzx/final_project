6bb6804f26c3b2683cf898805dc99099a8de7d26
Melwayz's <>
2020-07-02 10:33:20 +0700

    Insert link button to index

--- pages/covidScale.js
+++ pages/covidScale.js
@@ -1,12 +1,31 @@
 import Link from "next/link";
 import React from "react";
+import Button from "../src/components/Button";
 
 export default function covidScale() {
   return (
     <div>
-      {/* <Link href="/"></Link>
-      <a>Home</a> */}
-      <h1>HIHIHI</h1>
+      <Button
+        StyledButtonContainer={StyledButtonContainer}
+        StyledLink={StyledLink}
+        href={"/"}
+        name={"กลับไปหน้าหลัก"}
+      ></Button>
     </div>
   );
 }
+
+const StyledButtonContainer = {
+  width: "200px",
+  backgroundColor: "#C23D3D",
+  borderRadius: "8px",
+  color: "white",
+  padding: "10px",
+  boxSixing: "border-box",
+  textAlign: "center",
+};
+
+const StyledLink = {
+  color: "white",
+  textDecoration: "inherit",
+};
