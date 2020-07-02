c6cf8ad87e244f03008b65717c13a65431cd825a
Melwayz's <>
2020-07-02 11:35:14 +0700

    Create pie chart component

---
+++ src/components/Chart/PieChartData.js
@@ -0,0 +1,64 @@
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Cell } from "recharts";
+
+export default function PieChartData({ domesticSum }) {
+  console.log({ domesticSum });
+  const data = [
+    {
+      name: "ผู้หญิง",
+      value: domesticSum.Gender["Female"],
+    },
+    {
+      name: "ผู้ชาย",
+      value: domesticSum.Gender["Male"],
+    },
+  ];
+
+  const COLORS = ["#F08080", "#0088FE"];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  return (
+    <div>
+      <PieChart width={730} height={250}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          // outerRadius={80}
+          fill="#8884d8"
+        >
+          {data.map((entry, index) => (
+            <Cell fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </div>
+  );
+}
