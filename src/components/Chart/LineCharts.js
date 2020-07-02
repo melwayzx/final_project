49a8d9796573ad4264c9a6731830516d7563abb8
DESKTOP-PN1MIL9\valveronika <valveronika22@gmail.com>
2020-07-02 11:09:46 +0700

    Draft LneChart

---
+++ src/components/Chart/LineCharts.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+export default function LineCharts({ confirmGlobal }) {
+    //compare between date(x) and infected number(y)
+    // console.log(confirmGlobal)
+    const data = [{ confirmGlobal }]
+    return (
+        <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+                top: 5, right: 30, left: 20, bottom: 5,
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="confirmGlobal.['Country/Region'] " />
+            <YAxis dataKey="confirmGlobal.NewConfirmed" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="Thailand" stroke="#8884d8" />
+            <Line type="monotone" dataKey="Japan" stroke="#82ca9d" />
+        </LineChart>
+
+    );
+}
+
+
