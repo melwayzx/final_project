30a2c52adaf4363d552b08cc31aab8a0a8b5c85e
Melwayz's <>
2020-07-02 17:49:52 +0700

    Update table data with real data from API

--- src/components/Chart/Table.js
+++ src/components/Chart/Table.js
@@ -1,55 +1,44 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
-    table: {
-        minWidth: 600
-    },
+  table: {
+    minWidth: 600,
+  },
 });
 
-function createData(name, count) {
-    return { name, count };
-}
+export default function SimpleTable({ data }) {
+  const classes = useStyles();
 
-const rows = [
-    createData('กรุงเทพ', 1600),
-    createData('ภูเก็ต', 227),
-    createData('นนทบุรี', 158),
-    createData('สมุทรปราการ', 140),
-    createData('ยะลา', 134),
-];
-export default function SimpleTable() {
-    const classes = useStyles();
+  const provinceList = Object.keys(data.Province);
 
-    return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>จังหวัด</TableCell>
-                        <TableCell align="right">ผู้ติดเชื้อ</TableCell>
-
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.count}</TableCell>
-
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>จังหวัด</TableCell>
+            <TableCell align="right">ผู้ติดเชื้อ</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {provinceList.map((province) => (
+            <TableRow key={province}>
+              <TableCell component="th" scope="row">
+                {province}
+              </TableCell>
+              <TableCell align="right">{data.Province[province]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
