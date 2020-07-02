3dff4d3a56e0bf0706ad1fd97f4de9bc9f8d31a6
Melwayz's <>
2020-07-02 10:19:02 +0700

    Create covidScale page

---
+++ pages/covidScale.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function covidScale() {
+  return (
+    <div>
+      {/* <Link href="/"></Link>
+      <a>Home</a> */}
+      <h1>HIHIHI</h1>
+    </div>
+  );
+}
