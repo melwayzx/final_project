972949f8e706d3f15ebc7f05487c4780e62eaa07
Melwayz's <>
2020-07-07 13:10:29 +0700

    Map data from api

--- src/components/Chart/BarChartData.js
+++ src/components/Chart/BarChartData.js
@@ -10,22 +10,55 @@ import {
   Legend,
 } from "recharts";
 export default function BarChartData({ domesticCase }) {
-  console.log(domesticCase);
-  let age = [];
-  age = [{ ...domesticCase }].map((item) => console.log(item.Age));
-  console.log(age);
+  let age0_10 = 0;
+  let age11_20 = 0;
+  let age21_30 = 0;
+  let age31_40 = 0;
+  let age41_50 = 0;
+  let age51_60 = 0;
+  let age61_70 = 0;
+  let age71_80 = 0;
+  let age81_90 = 0;
+  let age91_100 = 0;
+  let ageUndefined = 0;
+
+  const age = domesticCase.Data.map((item) => {
+    if (item.Age <= 10) {
+      age0_10 += 1;
+    } else if (item.Age <= 20) {
+      age11_20 += 1;
+    } else if (item.Age <= 30) {
+      age21_30 += 1;
+    } else if (item.Age <= 40) {
+      age31_40 += 1;
+    } else if (item.Age <= 50) {
+      age41_50 += 1;
+    } else if (item.Age <= 60) {
+      age51_60 += 1;
+    } else if (item.Age <= 70) {
+      age61_70 += 1;
+    } else if (item.Age <= 80) {
+      age71_80 += 1;
+    } else if (item.Age <= 90) {
+      age81_90 += 1;
+    } else if (item.Age <= 100) {
+      age91_100 += 1;
+    } else {
+      ageUndefined += 1;
+    }
+  });
   const data = [
-    { name: "0-10 ปี", จำนวนคน: 62 },
-    { name: "11-20 ปี ", จำนวนคน: 121 },
-    { name: "21-30 ปี", จำนวนคน: 827 },
-    { name: "31-40 ปี", จำนวนคน: 771 },
-    { name: "41-50 ปี", จำนวนคน: 592 },
-    { name: "51-60 ปี", จำนวนคน: 433 },
-    { name: "61-70 ปี", จำนวนคน: 222 },
-    { name: "71-80 ปี", จำนวนคน: 86 },
-    { name: "81-90 ปี", จำนวนคน: 22 },
-    { name: "91-100 ปี", จำนวนคน: 2 },
-    { name: "ไม่ระบุ", จำนวนคน: 19 },
+    { name: "0-10 ปี", จำนวนคน: age0_10 },
+    { name: "11-20 ปี ", จำนวนคน: age11_20 },
+    { name: "21-30 ปี", จำนวนคน: age21_30 },
+    { name: "31-40 ปี", จำนวนคน: age31_40 },
+    { name: "41-50 ปี", จำนวนคน: age41_50 },
+    { name: "51-60 ปี", จำนวนคน: age51_60 },
+    { name: "61-70 ปี", จำนวนคน: age61_70 },
+    { name: "71-80 ปี", จำนวนคน: age71_80 },
+    { name: "81-90 ปี", จำนวนคน: age81_90 },
+    { name: "91-100 ปี", จำนวนคน: age91_100 },
+    { name: "ไม่ระบุ", จำนวนคน: ageUndefined },
   ];
   return (
     <BarChart
