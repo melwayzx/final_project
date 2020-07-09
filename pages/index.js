b0025ebce9db5094a95efadca6dfac36d0321d7c
Melwayz's <>
2020-07-09 14:03:15 +0700

    Import global chart

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,33 @@ import SumCard from "../src/components/Card/SumCard";
 import GenderSumCard from "../src/components/Card/GenderSumCard";
 import AgeSumCard from "../src/components/Card/AgeSumCard";
 import ThailandSumCard from "../src/components/Card/ThailandSumCard";
-// import SecondSection from "../src/components/section/SecondSection";
+import LineCharts from "../src/components/Chart/LineCharts";
+import GlobalSumCard from "../src/components/Card/GlobalSumCard";
+
 export default function index(props) {
-  const {
-    domesticDailyCase,
-    domesticSum,
-    domesticCase,
-    // confirmGlobal,
-    // deathGlobal,
-    // recoveredGlobal,
-  } = props;
-  // console.log(`Domestic daily case`, domesticDailyCase);
-  console.log(`domesticSum`, domesticSum);
+  const { domesticDailyCase, domesticSum, domesticCase } = props;
 
   return (
-    <div style={{ height: "100vh", width: "100vw", paddingTop: "150px" }}>
+    <div
+      style={{
+        width: "100vw",
+        paddingTop: "150px",
+        fontFamily: "Sukhumvit Set",
+      }}
+    >
       <Head>
         <title>Covid-19 updates </title>
       </Head>
-      <SumCard domesticDailyCase={domesticDailyCase} />
+      <div style={{ height: "250px" }}>
+        <SumCard domesticDailyCase={domesticDailyCase} />
+      </div>
       <div
         style={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           marginTop: "125px",
-          background: "#fafafafa",
+          background: "#FAFAFA",
           height: "600px",
         }}
       >
@@ -41,13 +42,25 @@ export default function index(props) {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          marginTop: "125px",
           background: "white",
-          height: "600px",
         }}
       >
         <ThailandSumCard domesticSum={domesticSum} />
       </div>
+      <div
+        style={{
+          // display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#FAFAFA",
+          height: "200px",
+          position: "absolute",
+          width: "100vw",
+        }}
+      >
+        <GlobalSumCard />
+      </div>
+      {/* <LineCharts /> */}
     </div>
   );
 }
@@ -56,18 +69,12 @@ export async function getServerSideProps() {
   const domesticDailyCase = await CovidReport.getDomesticDailyCase();
   const domesticSum = await CovidReport.getDomesticSum();
   const domesticCase = await CovidReport.getDomesticCase();
-  // const confirmGlobal = await CovidReport.getConfirmGlobal();
-  // const deathGlobal = await CovidReport.getDeathGlobal();
-  // const recoveredGlobal = await CovidReport.getRecoveredGlobal();
 
   return {
     props: {
       domesticDailyCase,
       domesticSum,
       domesticCase,
-      // confirmGlobal,
-      // deathGlobal,
-      // recoveredGlobal,]
     }, // will be passed to the page component as props
   };
 }
