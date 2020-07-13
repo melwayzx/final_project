f10dc183c3d9ed175500a8bb68e5d4637fb12f4e
Melwayz's <>
2020-07-14 04:37:45 +0700

    Pass country list data for selected box

--- src/components/SearchCountry.js
+++ src/components/SearchCountry.js
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
 import MultiSelect from "react-multi-select-component";
 
-export default function SearchCountry() {
-    const options = [
-        { label: "Thailand", value: "Thailand" },
-        { label: "USA", value: "USA" },
-        { label: "Japan", value: "Japan" },
-        { label: "Brazil", value: "Brazil" },
-        { label: "Turkey", value: "Turkey" },
-        { label: "India", value: "India" },
-        { label: "China", value: "China" },
-        { label: "Taiwan", value: "Taiwan" },
-        { label: "Italy", value: "Italy" },
-    ];
+export default function SearchCountry({ data }) {
+  //   const options = [
+  //     { label: "Thailand", value: "Thailand" },
+  //     { label: "USA", value: "USA" },
+  //     { label: "Japan", value: "Japan" },
+  //     { label: "Brazil", value: "Brazil" },
+  //     { label: "Turkey", value: "Turkey" },
+  //     { label: "India", value: "India" },
+  //     { label: "China", value: "China" },
+  //     { label: "Taiwan", value: "Taiwan" },
+  //     { label: "Italy", value: "Italy" },
+  //   ];
 
-    const [selected, setSelected] = useState([]);
+  //   const [selected, setSelected] = useState([]);
 
-    return (
-        <div>
-            <h1>เลือกประเทศ</h1>
-            <MultiSelect
-                options={options}
-                value={selected}
-                onChange={setSelected}
-                labelledBy={"Select"}
-
-            />
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>เลือกประเทศ</h1>
+      <MultiSelect
+        options={data}
+        // value={selected}
+        onChange={(e) => console.log(e)}
+        labelledBy={"Select"}
+      />
+    </div>
+  );
+}
