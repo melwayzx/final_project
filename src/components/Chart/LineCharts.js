fe838fdbc6910b7fddbd04fc7c370e98a305fe71
Melwayz's <>
2020-07-14 23:51:11 +0700

    Add selected country

--- src/components/Chart/LineCharts.js
+++ src/components/Chart/LineCharts.js
@@ -10,14 +10,23 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-import SearchCountry from "../SearchCountry";
+import MultiSelect from "react-multi-select-component";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  table: {
+    width: 300,
+  },
+});
 
 export default function LineCharts() {
   let data = [];
-  const newDates = [];
+  const initialData = ["Italy", "Brazil", "Iran"];
   const [mockData, setMockData] = useState();
   const [maxData, setMaxData] = useState(0);
   const [countryList, setCountryList] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     axios({
@@ -49,76 +58,112 @@ export default function LineCharts() {
         );
         setCountryList(unique);
 
-        // const newObj = { ...records };
-        // let dates = [];
-        // for (const key in newObj[0]) {
-        //   if (
-        //     key !== "Province/State" &&
-        //     key !== "Country/Region" &&
-        //     key !== "Lat" &&
-        //     key !== "Long"
-        //   ) {
-        //     dates.push(key);
-        //   }
-        // }
+        const newObj = { ...records };
+        let dates = [];
+        for (const key in newObj[0]) {
+          if (
+            key !== "Province/State" &&
+            key !== "Country/Region" &&
+            key !== "Lat" &&
+            key !== "Long"
+          ) {
+            dates.push(key);
+          }
+        }
 
-        // for (let i in dates) {
-        //   data.push({ date: dates[i] });
-        //   for (let j in records) {
-        //     if (records[j]["Country/Region"] === "Italy") {
-        //       data[i].Italy = records[j][dates[i]];
-        //     }
-        //     if (records[j]["Country/Region"] === "Brazil") {
-        //       data[i].Brazil = records[j][dates[i]];
-        //     }
-        //     if (records[j]["Country/Region"] === "Japan") {
-        //       data[i].Japan = records[j][dates[i]];
-        //     }
-        //   }
-        // }
+        if (selected.length == 0) {
+          for (let i in dates) {
+            data.push({ date: dates[i] });
+            for (let j in records) {
+              for (let k in initialData) {
+                if (records[j]["Country/Region"] === initialData[k]) {
+                  const name = initialData[k];
+                  data[i][name] = records[j][dates[i]];
+                }
+              }
+            }
+          }
+        } else {
+          for (let i in dates) {
+            data.push({ date: dates[i] });
+            for (let j in records) {
+              for (let k in selected) {
+                if (records[j]["Country/Region"] === selected[k].label) {
+                  const name = selected[k].label;
+                  data[i][name] = records[j][dates[i]];
+                }
+              }
+            }
+          }
+        }
 
-        // data = data.filter((item) => {
-        //   let cnt = 0;
-        //   for (const index in item) {
-        //     if (parseInt(item[index]) > 15000 && index !== 0) {
-        //       cnt += 1;
-        //     }
-        //     if (maxData < parseInt(item[index])) {
-        //       setMaxData(parseInt(item[index]));
-        //     }
-        //   }
-        //   return cnt === Object.keys(item).length - 1;
-        // });
+        const newData = [...data].filter((item) => {
+          let cnt = 0;
+          for (const index in item) {
+            if (parseInt(item[index]) > 100 && index !== 0) {
+              cnt += 1;
+            }
+            if (maxData < parseInt(item[index])) {
+              setMaxData(parseInt(item[index]));
+            }
+          }
+          return cnt === Object.keys(item).length - 1;
+        });
 
         setMockData(data);
-        // console.log(mockData);
+        console.log(mockData);
       })
 
       .catch((err) => {
         console.error(err);
       });
-  }, []);
+  }, [selected]);
+
+  function updateSelectCountry(e) {
+    // temp.push(e);
+    // console.log(temp);
+    setSelected(e);
+    console.log(selected);
+  }
 
   return (
     <div>
-      <SearchCountry data={countryList} />
+      <div style={{ width: "250px" }}>
+        {/* <h1>เลือกประเทศ</h1> */}
+        <MultiSelect
+          options={countryList}
+          value={selected}
+          onChange={function (e) {
+            updateSelectCountry(e);
+          }}
+          labelledBy={"Select"}
+        />
+      </div>
+      <div style={{ height: "1000px" }}>
+        <LineChart
+          width={1000}
+          height={300}
+          data={mockData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+          <YAxis type="number" domain={["auto", maxData]} />
+          <CartesianGrid stroke="#DDDDDD" vertical={false} />
+          <Tooltip />
+          <Legend />
+          {selected.length == 0
+            ? initialData.map((item) => (
+                <Line type="monotone" dataKey={item} stroke="#8884d8"></Line>
+              ))
+            : selected.map((item) => (
+                <Line
+                  type="monotone"
+                  dataKey={item.label}
+                  stroke="#8884d8"
+                ></Line>
+              ))}
+        </LineChart>
+      </div>
     </div>
-    // <div style={{ height: "1000px" }}>
-    //   <LineChart
-    //     width={1000}
-    //     height={300}
-    //     data={mockData}
-    //     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    //   >
-    //     <XAxis dataKey="date" tick={{ fontSize: 10 }} />
-    //     <YAxis type="number" domain={["auto", maxData]} />
-    //     <CartesianGrid stroke="#DDDDDD" vertical={false} />
-    //     <Tooltip />
-    //     <Legend />
-    //     <Line type="monotone" dataKey="Brazil" stroke="#8884d8" />
-    //     <Line type="monotone" dataKey="Italy" stroke="#82ca9d" />
-    //     <Line type="monotone" dataKey="Japan" stroke="#CE4A94" />
-    //   </LineChart>
-    // </div>
   );
 }
