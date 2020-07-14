a1827af778a1dcef6814f2a487f010052f644a2a
Melwayz's <>
2020-07-15 00:51:53 +0700

    Remove export function

--- src/services/covid-reports/index.js
+++ src/services/covid-reports/index.js
@@ -1,15 +1,9 @@
 import getDomesticDailyCase from "./getDomesticDailyCase";
 import getDomesticSum from "./getDomesticSum";
-import getRecoveredGlobal from "./getRecoveredGlobal";
-import getDeathGlobal from "./getDeathGlobal";
-import getConfirmGlobal from "./getConfirmGlobal";
 import getDomesticCase from "./getDomesticCase";
 
 export default {
   getDomesticDailyCase,
   getDomesticSum,
-  getRecoveredGlobal,
-  getDeathGlobal,
-  getConfirmGlobal,
   getDomesticCase,
 };
